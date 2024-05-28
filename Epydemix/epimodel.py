01429f19da8aa45e7fc356d04b8a57d9da795f86
ngozzi <nic.gozzi@gmail.com>
2024-05-28 09:19:47 +0200

    added check on compartments in epimodel

--- Epydemix/epimodel.py
+++ Epydemix/epimodel.py
@@ -6,14 +6,31 @@ class EpiModel:
     EpiModel Class
     """
 
-    def __init__(self):
+    def __init__(self, compartments=[]):
         """
         EpiModel constructor
         """
         self.transitions = {}
         self.transitions_list = []
         self.interventions = []
+        self.compartments = []
+        self.add_compartments(compartments)
 
+    def add_compartments(self, compartments): 
+        """
+        Adds compartments to the epidemic model.
+
+        Parameters:
+        -----------
+            - compartments (list or object): A list of compartments or a single compartment to be added.
+        """
+        if isinstance(compartments, list):
+            self.compartments.extend(compartments)
+            for compartment in compartments:
+                self.transitions[compartment] = []
+        else:
+            self.compartments.append(compartments)
+            self.transitions[compartments] = []
 
     def add_transition(self, source, target, rate, rate_name, agent=None):
         """
@@ -26,14 +43,21 @@ class EpiModel:
             - rate (float): The rate at which the transition occurs.
             - rate_name (str): The name of the rate for the transition.
             - agent (str, optional): The interacting agent for the transition (default is None).
+
+        Raises:
+        -------
+            - ValueError: If the source, target, or agent (if provided) is not in the compartments list.
         """
+
+        # Check that the source and target are in the compartment list
+        missing_compartments = [comp for comp in [source, target, agent] if comp and comp not in self.compartments]
+        if missing_compartments:
+            raise ValueError(f"These compartments are not in the compartments list: {', '.join(missing_compartments)}")
+
         transition = Transition(source=source, target=target, rate=rate, rate_name=rate_name, agent=agent)
 
         # Append to transitions list
         self.transitions_list.append(transition)
 
-        # Add transition to the dictionary, initializing list if necessary
-        if source in self.transitions:
-            self.transitions[source].append(transition)
-        else:
-            self.transitions[source] = [transition]
+        # Add transition to the dictionary
+        self.transitions[source].append(transition)
