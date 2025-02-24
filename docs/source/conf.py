329dc260318581a849b5274a93c7eaf6bc89aaaf
ngozzi <nic.gozzi@gmail.com>
2025-02-24 18:35:13 +0100

    fixing doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,33 +2,23 @@ import os
 import sys
 import sphinx_rtd_theme
 
-sys.path.insert(0, os.path.abspath('../..'))  # adjust path to point to your package root
-
+sys.path.insert(0, os.path.abspath('../..'))  
 
 project = 'epydemix'
 copyright = '2025, Nicolò Gozzi'
 author = 'Nicolò Gozzi'
 release = '1.0.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = [
-    "sphinx.ext.autodoc",       # Extracts documentation from docstrings
-    "sphinx.ext.napoleon",      # Supports Google and NumPy-style docstrings
-    "sphinx.ext.viewcode",      # Adds links to view source code
-    "sphinx.ext.autosummary",   # Automatically generates summaries
+    "sphinx.ext.autodoc",      
+    "sphinx.ext.napoleon",      
+    "sphinx.ext.viewcode",      
+    "sphinx.ext.autosummary",  
 ]
 
-# Enable autosummary
-autosummary_generate = True  # Generate stub `.rst` files for API documentation
-
+autosummary_generate = True  
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
